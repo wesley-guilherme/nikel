6c32ffc29a20d24f3308ade5c91177e181bcbcb4
Wesley <wesley.analistasystem@gmail.com>
2025-11-23 20:18:01 -0300

    quinto

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,36 @@
 const myModal = new bootstrap.Modal("#registerModal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+chekLogged();
+
+//LOGAR NO SISTEMA
+document.getElementById("login-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+    const password = document.getElementById("password-input").value;
+    const checkSession =  document.getElementById("session-check").checked;
+
+    const account = getAccount(email);
+
+    if(!account) {
+        alert("Ops!!! Verifique o usuário ou a senha")
+        return;
+    }
+
+    if(account) {
+        if(account.password !== password) {
+            alert("Ops!!! Verifique o usuário ou a senha")
+            return;
+        }
+
+    saveSession(email, checkSession)
+
+    window.location.href = "home.html";        
+    }
+
+});
 
 //CRIAR CONTA
 document.getElementById("create-form").addEventListener("submit", function(e) {
@@ -16,7 +48,6 @@ document.getElementById("create-form").addEventListener("submit", function(e) {
         alert("Senha curta precisa ser 5 dígitos a mais");
         return;
     }
-
     saveAccount({
         login: email,
         password: password,
@@ -28,6 +59,36 @@ document.getElementById("create-form").addEventListener("submit", function(e) {
     alert("Conta criada com sucesso");
 });
 
+    function chekLogged() {
+        if(session) {
+            sessionStorage.setItem("logged", session)
+            logged = session
+        }
+
+        if(logged) {
+            saveSession(logged, session);
+
+           window.location.href = "home.html" 
+        }
+    }
+
 function saveAccount(data) {
-    localStorage.setItem(data.login, JSON.stringify)
+    localStorage.setItem(data.login, JSON.stringify(data))}
+
+function saveSession(data, saveSession) {
+    if(saveSession) {
+        localStorage.setItem("session", data)
+    }
+
+    sessionStorage.setItem("logged", data)
+}
+
+    function getAccount(key) {
+    const account = localStorage.getItem(key);
+
+    if(account) {
+        return JSON.parse(account);
+    }
+
+    return "";
 }
