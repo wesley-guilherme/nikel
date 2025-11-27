bcf0a591fc7b4012fd1f66828d5d78aca43ee7f0
Wesley <wesley.analistasystem@gmail.com>
2025-11-26 22:11:37 -0300

    261125

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -1,4 +1,4 @@
-const myModal = new bootstrap.Modal("#transaction-modal");
+const myModal = new bootstrap.Modal(document.getElementById("transaction-modal"));
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
 
